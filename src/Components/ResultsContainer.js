65023af71a910421ee2e1fa54968c60d44886743
github username <rubyclaroreilly@gmail.com>
2019-01-15 21:57:55 -0500

    first commit

---
+++ src/Components/ResultsContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ResultsContainer extends Component{
+
+  render(){
+    return(
+      <div>ResultsContainer (generate result cards)</div>
+    )
+  }
+}
+
+export default ResultsContainer
